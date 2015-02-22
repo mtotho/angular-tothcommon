cceff9fa424bbf31ee04b633c85601845f29f3a1
Michael Toth <mtotho@gmail.com>
2015-02-21 19:09:53 -0500

    added binding debugging

--- tothcommon.js
+++ tothcommon.js
@@ -50,6 +50,43 @@ angular.module('tothcommon.diagnostics', [
 
 
 }(angular.module("tothcommon.diagnostics")));
+(function(module){
+
+    module.config(function($provide){
+
+        $provide.decorator("$interpolate", function($delegate, $log){
+
+            var serviceWrapper = function(){
+                var bindingFunction = $delegate.apply(this, arguments);
+                if(angular.isFunction(bindingFunction) && arguments[0]){
+                    return bindingWrapper(bindingFunction, arguments[0].trim());
+                }
+
+                return bindingFunction;
+            };
+
+            var bindingWrapper = function(bindingFunction, bindingExpression){
+                return function(){
+                    var result = bindingFunction.apply(this, arguments);
+                    var trimmedResult = result.trim();
+                 
+
+                    var log = trimmedResult ? $log.info : $log.warn;
+                    log.call($log, bindingExpression + " = " + trimmedResult)
+                    return result;
+                }
+            }
+            angular.extend(serviceWrapper, $delegate)
+
+            return serviceWrapper;
+
+        });
+    });
+
+
+
+
+}(angular.module("tothcommon.diagnostics")));
 'use strict';
 
 (function(module){
@@ -125,6 +162,49 @@ angular.module('tothcommon.diagnostics', [
 
 (function(module){
 
+    var CurrentUser = function(localStorage){
+
+        var USERKEY = "utoken";
+
+        var setProfile = function(username, token){
+            profile.username=username;
+            profile.token = token;
+            localStorage.add(USERKEY, profile);
+        }
+
+        var initialize = function(){
+
+            var user ={
+                username:"",
+                token:"",
+                get loggedIn() {
+                    return this.token;
+                }
+            }
+            var localUser = localStorage.get(USERKEY);
+            if(localUser){
+                user.username = localUser.username;
+                user.token = localUser.token;
+            }
+
+            return user;
+        }
+
+        var profile = initialize();
+
+        return{
+            setProfile:setProfile,
+            profile:profile
+        }
+    };
+
+    module.factory("CurrentUser", CurrentUser);
+
+}(angular.module("tothcommon")));
+'use strict';
+
+(function(module){
+
     var addToken = function(CurrentUser, $q){
 
         //If the user is logged in on an HTTP request, add their token to headers
@@ -181,48 +261,5 @@ angular.module('tothcommon.diagnostics', [
     module.config(function($httpProvider){
         $httpProvider.interceptors.push("loginRedirect");
     });
-
-}(angular.module("tothcommon")));
-'use strict';
-
-(function(module){
-
-    var CurrentUser = function(localStorage){
-
-        var USERKEY = "utoken";
-
-        var setProfile = function(username, token){
-            profile.username=username;
-            profile.token = token;
-            localStorage.add(USERKEY, profile);
-        }
-
-        var initialize = function(){
-
-            var user ={
-                username:"",
-                token:"",
-                get loggedIn() {
-                    return this.token;
-                }
-            }
-            var localUser = localStorage.get(USERKEY);
-            if(localUser){
-                user.username = localUser.username;
-                user.token = localUser.token;
-            }
-
-            return user;
-        }
-
-        var profile = initialize();
-
-        return{
-            setProfile:setProfile,
-            profile:profile
-        }
-    };
-
-    module.factory("CurrentUser", CurrentUser);
 
 }(angular.module("tothcommon")));
